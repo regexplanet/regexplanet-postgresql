0e66436860c6116cae06eb23a7d23f444d7542ff
Andrew Marcuse <fileformat@gmail.com>
2024-10-25 18:17:16 -0400

    Updated to latest regexplanet-common

--- src/server.js
+++ src/server.js
@@ -67,24 +67,24 @@ const runTest = async (testInput) => {
 
     html.push("</table>\n");
 
-    const pg = new PGlite();
-    await pg.exec(createTable);
-    for (let index = 0; index < testInput.inputs.length; index++) {
-        const input = testInput.inputs[index];
-        if (!input) {
-            continue;
+    if (testInput.inputs && testInput.inputs.length > 0) {
+        const pg = new PGlite();
+        await pg.exec(createTable);
+        for (let index = 0; index < testInput.inputs.length; index++) {
+            const input = testInput.inputs[index];
+            if (!input) {
+                continue;
+            }
+            await pg.query(
+                "INSERT INTO rxp_test (id, input, regex, replacement) VALUES ($1, $2, $3, $4)",
+                [index, input, testInput.regex, testInput.replacement]
+            );
         }
-        await pg.query(
-            "INSERT INTO rxp_test (id, input, regex, replacement) VALUES ($1, $2, $3, $4)",
-            [index + 1, input, testInput.regex, testInput.replacement]
-        );
-    }
 
-    const debugData = await pg.query("SELECT * FROM rxp_test");
-    console.log(debugData);
+        const debugData = await pg.query("SELECT * FROM rxp_test");
 
-    const resultData = await pg.query(
-        `SELECT
+        const resultData = await pg.query(
+            `SELECT
                 (id+1)::varchar AS id,
                 input,
                 (input SIMILAR TO regex)::varchar AS similar_to,
@@ -95,75 +95,75 @@ const runTest = async (testInput) => {
                 substring(input from regex) AS substring_from,
                 regexp_replace(input, regex, replacement) AS replace
                 FROM rxp_test`
-    );
-    console.log(resultData);
+        );
 
-    const matchData = await pg.query(`SELECT
+        const matchData = await pg.query(`SELECT
         (id+1)::varchar AS id,
         UNNEST(regexp_matches(input, regex))
         FROM rxp_test`);
-    console.log(matchData);
-    /*for (const row of matchData.rows) {
+        //console.log(matchData);
+        /*for (const row of matchData.rows) {
         if row[0] in matches {
             matches[row[0]].append(row[1])
         else:
             matches[row[0]] = [ row[1] ]
 */
-    html.push('<table class="table table-bordered table-striped">\n');
-    html.push("\t<thead>\n");
-    html.push("\t\t<tr>\n");
-    html.push('\t\t\t<th style="text-align:center;">Test</th>\n');
-    html.push("\t\t\t<th>Target String</th>\n");
-    html.push("\t\t\t<th>SIMILAR TO</th>\n");
-    html.push("\t\t\t<th>~</th>\n");
-    html.push("\t\t\t<th>~*</th>\n");
-    html.push("\t\t\t<th>!~</th>\n");
-    html.push("\t\t\t<th>!~*</th>\n");
-    html.push("\t\t\t<th>substring()</th>\n");
-    html.push("\t\t\t<th>regex_replace()</th>\n");
-    html.push("\t\t\t<th>regex_matches()</th>\n");
-    html.push("\t\t</tr>");
-    html.push("\t</thead>");
-    html.push("\t<tbody>");
-
-    for (const row of resultData.rows) {
+        html.push('<table class="table table-bordered table-striped">\n');
+        html.push("\t<thead>\n");
         html.push("\t\t<tr>\n");
-        html.push('\t\t\t<td style="text-align:center">');
-        html.push(h(row.id));
-        html.push("</td>\n");
+        html.push('\t\t\t<th style="text-align:center;">Test</th>\n');
+        html.push("\t\t\t<th>Target String</th>\n");
+        html.push("\t\t\t<th>SIMILAR TO</th>\n");
+        html.push("\t\t\t<th>~</th>\n");
+        html.push("\t\t\t<th>~*</th>\n");
+        html.push("\t\t\t<th>!~</th>\n");
+        html.push("\t\t\t<th>!~*</th>\n");
+        html.push("\t\t\t<th>substring()</th>\n");
+        html.push("\t\t\t<th>regex_replace()</th>\n");
+        html.push("\t\t\t<th>regex_matches()</th>\n");
+        html.push("\t\t</tr>");
+        html.push("\t</thead>");
+        html.push("\t<tbody>");
 
-        for (const col of [
-            "input",
-            "similar_to",
-            "tilde",
-            "tilde_star",
-            "not_tilde",
-            "not_tilde_star",
-            "substring_from",
-            "replace",
-        ]) {
-            html.push("\t\t\t<td>");
-            html.push(h(row[col]));
+        for (const row of resultData.rows) {
+            html.push("\t\t<tr>\n");
+            html.push('\t\t\t<td style="text-align:center">');
+            html.push(h(row.id));
             html.push("</td>\n");
-        }
 
-        html.push("\t\t\t<td>");
-        /*if row[0] not in matches:
+            for (const col of [
+                "input",
+                "similar_to",
+                "tilde",
+                "tilde_star",
+                "not_tilde",
+                "not_tilde_star",
+                "substring_from",
+                "replace",
+            ]) {
+                html.push("\t\t\t<td>");
+                html.push(h(row[col]));
+                html.push("</td>\n");
+            }
+
+            html.push("\t\t\t<td>");
+            /*if row[0] not in matches:
                 html.push("<i>(none)</i>")
             else:
                 matchlist = matches[row[0]]
                 for matchloop in range(0, len(matchlist)):
                     html.push("%d: %s<br/>" % (matchloop+1, safe_escape(matchlist[matchloop])))
             */
-        html.push("</td>\n");
+            html.push("</td>\n");
 
-        html.push("\t\t</tr>\n");
+            html.push("\t\t</tr>\n");
+        }
+
+        html.push("\t</tbody>\n");
+        html.push("</table>\n");
+
+        await pg.close();
     }
-
-    html.push("\t</tbody>\n");
-    html.push("</table>\n");
-
-    await pg.close();
 
     return {
         success: true,
